7d7b317b25c6825a498390f14c3d6717f236cc04
Jean-Luc Fattebert <fattebertj@ornl.gov>
2020-10-14 14:38:21 -0400

    Use Catch for testing

--- tests/property_tree/testPropertyTree.cc
+++ tests/property_tree/testPropertyTree.cc
@@ -1,3 +1,7 @@
+#define CATCH_CONFIG_MAIN
+
+#include "../catch.hpp"
+
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
@@ -7,7 +11,9 @@
 
 namespace pt = boost::property_tree;
 
-int main(int argc, char* argv[])
+// Simply check that we are able to read correctly a few
+// numbers from JSON file
+TEST_CASE("Simple JSON read", "[json read]")
 {
     std::string input_filename("test.json");
 
@@ -32,4 +38,10 @@ int main(int argc, char* argv[])
     for (auto tc : Tcs)
         std::cout << " " << tc;
     std::cout << std::endl;
+
+    REQUIRE(Tcs.size() == 5);
+
+    REQUIRE(Tcs[0] == Approx(298.15).epsilon(1.e-12));
+
+    REQUIRE(Tcs[4] == Approx(3000.0).epsilon(1.e-12));
 }
