47c6bb8a16a728d7cb8b425a5500ce72907f99cc
DeWitt, Stephen <dewittsj@ornl.gov>
2021-07-02 15:32:49 -0400

    added a missing non-offload guard in PhysicalConstants.h

--- src/PhysicalConstants.h
+++ src/PhysicalConstants.h
@@ -3,10 +3,13 @@
 
 namespace Thermo4PFM
 {
-
+#ifdef HAVE_OPENMP_OFFLOAD
 #pragma omp declare target
+#endif
 const double gas_constant_R_JpKpmol = 8.314472; // J K-1 mol-1
+#ifdef HAVE_OPENMP_OFFLOAD
 #pragma omp end declare target
+#endif
 }
 
 #endif
