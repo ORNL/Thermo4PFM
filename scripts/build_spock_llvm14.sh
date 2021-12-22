326f6823c896997963e43665f1b5b1ea37f4884f
Jean-Luc Fattebert <fattebertj@ornl.gov>
2021-12-22 11:31:18 -0500

    Fix build script for LLVM14

--- scripts/build_spock_llvm14.sh
+++ scripts/build_spock_llvm14.sh
@@ -1,5 +1,4 @@
 #!/bin/csh -f
-#setenv LLVM_ROOT /sw/spock/ums/eiw/llvm/14.0.0-20210819
 setenv LLVM_ROOT /sw/spock/ums/eiw/llvm/14.0.0-20211207
 setenv LD_LIBRARY_PATH ${LLVM_ROOT}/lib:/opt/rocm-4.2.0/lib:/opt/rocm-4.2.0/lib64:$LD_LIBRARY_PATH
 setenv PATH ${LLVM_ROOT}/bin:$PATH
@@ -20,12 +19,13 @@ cd ${BUILD_DIR}
 
 # call cmake
 cmake -DCMAKE_CXX_COMPILER=clang++ \
-      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_BUILD_TYPE=Debug \
       -DWITH_OPENMP_OFFLOAD=ON \
       -DCMAKE_CXX_FLAGS="-O2 -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -std=c++11 -D__NO_MATH_INLINES -U__SSE2_MATH__ -U__SSE_MATH__" \
       -DWITH_OPENMP_OFFLOAD=ON \
       -DMPIEXEC_EXECUTABLE="srun" \
       -DMPIEXEC_NUMPROCS_FLAG="-n" \
+      -DMPIEXEC_NUMPROCS="1" \
       -DCMAKE_EXE_LINKER_FLAGS="-lm -fopenmp" \
       ..
 
