7e8e1bc248077315ef165ef5ca36e8b14b15263e
Jean-Luc Fattebert <fattebertj@ornl.gov>
2021-12-22 15:17:55 -0500

    Fix summit LLVM build script

--- scripts/build_summit_llvm14.sh
+++ scripts/build_summit_llvm14.sh
@@ -23,9 +23,10 @@ cmake -DCMAKE_CXX_COMPILER=clang++ \
       -DWITH_OPENMP_OFFLOAD=ON \
       -DCMAKE_CXX_FLAGS="-fopenmp -fopenmp-targets=nvptx64 -Rpass=openmp-opt -Rpass-missed=openmp-opt" \
       -DWITH_OPENMP_OFFLOAD=ON \
-      -DMPIEXEC_EXECUTABLE="srun" \
+      -DMPIEXEC_EXECUTABLE="/sw/summit/xalt/1.2.1/bin/jsrun" \
       -DMPIEXEC_NUMPROCS_FLAG="-n" \
       -DMPIEXEC_NUMPROCS="1" \
+      -DMPIEXEC_PREFLAGS="-a1;-c4;-bpacked:2;-g1" \
       -DCMAKE_EXE_LINKER_FLAGS="-lm -fopenmp" \
       ..
 
