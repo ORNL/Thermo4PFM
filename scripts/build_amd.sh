c4c588e808f0ad9ea391899a9fbc1b7429154d74
Jean-Luc Fattebert <fattebertj@ornl.gov>
2023-04-28 14:57:47 -0400

    Add build scripts for Frontier@OLCF (#52)

---
+++ scripts/build_amd.sh
@@ -0,0 +1,28 @@
+#!/bin/csh
+module load PrgEnv-amd
+module load craype-accel-amd-gfx90a
+module load boost
+module load cmake
+module load cray-python
+
+set ROOT = `pwd`
+
+set BUILD_DIR = ${ROOT}/build
+rm -rf ${BUILD_DIR}
+
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+# call cmake
+cmake -DCMAKE_CXX_COMPILER=CC \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DWITH_OPENMP_OFFLOAD=ON \
+      -DCMAKE_CXX_FLAGS="-fopenmp -fopenmp-assume-no-thread-state -fopenmp-cuda-mode -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=gfx90a" \
+      -DMPIEXEC_EXECUTABLE="srun" \
+      -DMPIEXEC_NUMPROCS_FLAG="-n" \
+      -DMPIEXEC_NUMPROCS="1" \
+      -DMPIEXEC_PREFLAGS="-c1;--gpus=1" \
+      -DCMAKE_EXE_LINKER_FLAGS="-lm -fopenmp" \
+      ..
+
+make
